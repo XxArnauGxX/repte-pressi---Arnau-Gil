16656f7ccb31f3d0d158122a809c24d219b661ae
arnauggX <lpac7697@gmail.com>
2024-03-12 12:14:05 +0100

    V3

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class Main {
     public static void main(String[] args) {
-
-        FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
-
-        System.out.println("¡Configuración de Apache FOP completada!");
+        System.out.println("Versión 3: Creación del archivo de salida.");
     }
 }
