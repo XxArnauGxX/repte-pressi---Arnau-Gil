9b75c2120375b209642da385e17a26da443dc1aa
arnauggX <lpac7697@gmail.com>
2024-03-12 12:16:01 +0100

    V4

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.MimeConstants;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.Result;
+import javax.xml.transform.sax.SAXResult;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -7,6 +13,6 @@ import java.io.OutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Versión 3: Creación del archivo de salida.");
+        System.out.println("Hello world!!!");
     }
 }
